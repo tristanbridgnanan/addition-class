2d73862183246f6ebe89d246ef4cb34079faac01
TRISTAN BRIDGNANAN <tristanbridgnanan@adelphi.internal>
2016-09-20 11:01:09 -0400

    addtion

---
+++ Addition/Addnum.java
@@ -0,0 +1,15 @@
+// Adding Two Numbers
+// Description: It reads out the addtion of two numbes 
+// Input: Double 
+// Output: Double 
+// Example: 1+1=2
+public class Addnum
+{ 
+    private double num;
+    
+    public Addnum(double num1, double num2)
+    {
+        num = num1 + num2;
+        System.out.println("Sum of two numbers =" + num);
+    }
+}

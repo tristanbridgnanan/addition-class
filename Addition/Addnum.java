dfa8b5aec9989fe869ed6ca2eb4fd0d91bd81656
tristanbridgnanan <tristandbridgnanan@gmail.com>
2016-09-20 11:13:53 -0400

    Update Addnum.java

--- Addition/Addnum.java
+++ Addition/Addnum.java
@@ -1,15 +1,26 @@
 // Adding Two Numbers
-// Description: It reads out the addtion of two numbes 
-// Input: Double 
-// Output: Double 
-// Example: 1+1=2
+
 public class Addnum
 { 
     private double num;
     
+     // Description: It reads out the addtion of two numbes 
+     // Input: Double 
+     // Output: Double 
+     // Example: 1+1=2
+     
+     // This will allow for an input of two numbers that will prompt an output
+    
     public Addnum(double num1, double num2)
     {
         num = num1 + num2;
+    }
+    
+    // This will show the sum of the two numbers that were entered
+    
+    public void showanswer()
+    {
         System.out.println("Sum of two numbers =" + num);
     }
+        
 }
